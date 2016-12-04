4a4ab5e81c5aa4716192680ec8ec7f0436fc9b7f
Jong Yon Kim <jongyonkim@gmail.com>
2016-12-04 00:24:22 -0800

    Add second function to trigger Travis CI

--- code.py
+++ code.py
@@ -4,3 +4,6 @@ import requests
 
 def add_one(val):
 	return val + 1
+
+def add_two(val):
+	return val + 2
--- test_code.py
+++ test_code.py
@@ -4,3 +4,6 @@ import code
 
 def test_add_one():
 	assert code.add_one(1) == 2
+
+def test_add_two():
+	assert code.add_two(1) == 3
